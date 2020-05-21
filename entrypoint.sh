ec5bf6f05919062e2cf7d365adb342af436fcb1a
Barak Schoster Goihman <barak@bridgecrew.io>
2020-05-21 11:26:12 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@
 #
 [[ ! -z "$INPUT_CHECK" ]] && CHECK_FLAG="--check $INPUT_CHECK"
 [[ ! -z "$INPUT_SKIP_CHECK" ]] && SKIP_CHECK_FLAG="--skip-check $INPUT_SKIP_CHECK"
-ls /usr/local/lib/problem-matcher.json
+
+matcher_path=`pwd`/checkov-problem-matcher.json
+
+cp /usr/local/lib/checkov-problem-matcher.json "$matcher_path"
+
 echo "::add-matcher::/usr/local/lib/problem-matcher.json"
 echo "running checkov on directory: $1"
 checkov -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG
