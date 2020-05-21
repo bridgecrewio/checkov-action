1b0c5b59561f3a573242ace2f634fbf0210e9778
barak <schosterbarak@gmail.com>
2020-05-21 08:35:42 +0300

    add problem matcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 #
 [[ ! -z "$INPUT_CHECK" ]] && CHECK_FLAG="--check $INPUT_CHECK"
 [[ ! -z "$INPUT_SKIP_CHECK" ]] && SKIP_CHECK_FLAG="--skip-check $INPUT_SKIP_CHECK"
+echo "::add-matcher::./problem-matcher.json"
 echo "running checkov on directory: $1"
 checkov -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG
