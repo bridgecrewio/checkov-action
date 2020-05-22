8cb0132c8a16b0ddda9421eeb4f1799bbc0b5afe
twmartin <ty.w.martin@gmail.com>
2020-05-22 11:10:07 -0500

    Allow for quiet output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 #
 [[ ! -z "$INPUT_CHECK" ]] && CHECK_FLAG="--check $INPUT_CHECK"
 [[ ! -z "$INPUT_SKIP_CHECK" ]] && SKIP_CHECK_FLAG="--skip-check $INPUT_SKIP_CHECK"
+[[ ! -z "$INPUT_QUIET" ]] && QUIET_FLAG="--quiet"
 
 matcher_path=`pwd`/checkov-problem-matcher.json
 
@@ -10,4 +11,4 @@ cp /usr/local/lib/checkov-problem-matcher.json "$matcher_path"
 
 echo "::add-matcher::checkov-problem-matcher.json"
 echo "running checkov on directory: $1"
-checkov -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG
+checkov -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG
